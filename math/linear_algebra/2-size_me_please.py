44add0ba934b433e6f04458552e3fc396ce085ca
kendihasko <khasko16@epoka.edu.al>
2024-01-23 16:24:21 +0000

    Task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+"""
+A function def matrix_shape(matrix): that calculates the shape of a matrix
+"""
+
 
 def matrix_shape(matrix):
+    """
+A function def matrix_shape(matrix): that calculates the shape of a matrix
+    """
+
     matrix_shape = []
     while type(matrix) is list:
         matrix_shape.append(len(matrix))
