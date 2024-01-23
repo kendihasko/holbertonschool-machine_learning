32875c9024ba6a6f779ced776e184c41cdbac9e6
kendihasko <khasko16@epoka.edu.al>
2024-01-23 16:19:51 +0000

    Task 2

--- math/linear_algebra/test.py
+++ math/linear_algebra/test.py
@@ -14,3 +14,14 @@ for row in matrix: #zakonisht keshtu iterojme ne matrixes
 
 
 #Task 1 - iterate on each row me rradhe, append colums 3,4 to middle
+# cat emri.py - shfaq permbajtjen e file-it
+#chmod u+x *.py - per gjithe filet, pasi e krijon filein ||  e ben file-in qe e ka chmod ne rregull dhe e kopjon, fshin permbajtjen dhe e perdor si file te ri - cp name.py newfile.py
+#te Task 2 matrix eshte 3 dimensionale, prap lista 1 , e verdha ka x roze, roza ka x blu dhe blu ka x elemente || sa kllapa ke
+#numri i rreshtave i matrix eshte len(), cols = len(matrix[0])
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    return (rows, cols)
+
+
+    #duhet ti zvogelojme dimensionin?
