f60a4f2fd7c9801ec0130b177ee921e8b82c4815
kendihasko <khasko16@epoka.edu.al>
2024-01-30 14:36:29 +0000

    Task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A script that transposes the matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+A function that transposes the matrix
+    """
+    return matrix.transpose()
