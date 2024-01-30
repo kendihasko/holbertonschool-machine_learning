cf1ed1d14cbbc577230de04a6c64760b8733aff0
kendihasko <khasko16@epoka.edu.al>
2024-01-30 13:49:07 +0000

    Task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+...
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+...
+    """
+arr1 = []
+arr2 = []
+
+concaterates_list = arr1 + arr2
+return concaterates_list
+
+result = cat_arrays((arr1), arr2)
+print(result)
