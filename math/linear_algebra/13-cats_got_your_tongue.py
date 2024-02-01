a9c9ca5546ba21bd21e8b0f0024ea2583d8c07b8
kendihasko <khasko16@epoka.edu.al>
2024-02-01 12:04:36 +0000

    Task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,6 +4,8 @@ A script that concatenates two matrices along a specific axis
 """
 
 import numpy as np
+
+
 def np_cat(mat1, mat2, axis=0):
     """
 A function that concatenates two matrices along a specific axis
