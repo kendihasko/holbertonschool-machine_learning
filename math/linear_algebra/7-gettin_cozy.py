9d9af14afcff40e419f38e5e29e2aa2f1e82850d
kendihasko <khasko16@epoka.edu.al>
2024-02-01 11:37:00 +0000

    Task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-...
+A script that concatenates two matrices along a specific axis
 """
 
 def cat_matrices2D(mat1, mat2, axis=0): 
@@ -11,5 +11,18 @@ A function that concatenates two matrices along a specific axis
     if axis == 0:
         if len(mat1[0] != len(mat2[0])):
             return None
-
-            
+        cat_matrix = []
+        for row in mat1:
+            cat_matrix.append(list(row))
+        for row in mat2:
+            cat_matrix.append(list(row))
+        return cat_matrix
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        cat_matrix = None
+        for i in range(len(mat1)):
+            cat_matrix.append(list(mat1[i]) + list(mat2[i]))
+            return matrix
+    else:
+        return None
