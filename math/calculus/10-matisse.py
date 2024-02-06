b7a0b719460ad99b1f6c7facbeff1f68d22d5ad3
kendihasko <khasko16@epoka.edu.al>
2024-02-06 13:22:31 +0000

    Task 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -13,11 +13,13 @@ def poly_derivative(poly):
     for coeff in poly:
         if type(coeff) is not int:
             return None
-    
+
     for power, coefficient in enumerate(poly):
         if power == 0:
             derivative = [0]
         if power == 1:
             derivative = []
         derivative.append(power * coefficient)
+    while derivative[-1] == 0 and len(derivative) > 1:
+        derivative = derivative[:-1]
     return derivative
