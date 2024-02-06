7e9f5d3b1914c5d08de8a25399ecdf5223516dca
kendihasko <khasko16@epoka.edu.al>
2024-02-06 13:18:14 +0000

    Task 10

--- math/calculus/test2.py
+++ math/calculus/test2.py
@@ -5,3 +5,8 @@ def summation_i_squared(n):
         print(f"i ={1}, i^2 = {i**2}, sum = {sum}")
 
     return sum
+
+    #kur permend validitetin mund te jete per llojin e variablit nese eshte sic e kerkon tasku
+
+       for power, coefficient enumerate(poly):
+        #print(f"{power}:{coefficient}")
