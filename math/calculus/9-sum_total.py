81510766a295c67e7cb69d48264b32ccb3647341
kendihasko <khasko16@epoka.edu.al>
2024-02-06 12:57:20 +0000

    Task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,7 +8,7 @@ def summation_i_squared(n):
     '''
     A function that calculates the sum of squares
     '''
-    if type(n) is not int:
+    if type(n) is not int or n < 1:
         return None
     sum = (n*(n+1)*(2*n+1))/6
-    return sum
+    return int(sum)
