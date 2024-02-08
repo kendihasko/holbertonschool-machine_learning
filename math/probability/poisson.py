30ce2af92059049899811e550cc2bace17c31610
kendihasko <khasko16@epoka.edu.al>
2024-02-08 15:02:32 +0000

    Task 0

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 '''
-A class thst represemts a poisson distribution.
+A class that represemts a poisson distribution.
 '''
 
 
 class Poisson:
     '''
-A class thst represemts a poisson distribution.
+A class that represemts a poisson distribution.
     '''
     def __init__(self, data=None, lambtha=1.):
         self.lambtha = float(lambtha)
-        if self.lambtha < 0:
+        if self.lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
         if data is not None:
             if type(data) is not list:
@@ -18,3 +18,31 @@ A class thst represemts a poisson distribution.
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data)) / len(data)
+
+    def pmf(self, k):
+        '''
+        A method that calculates the value of the PMF for a given number of “successes”
+        '''
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        e = 2.7182818285    
+        factorial = 1
+        for i in range(k):
+            factorial *= (k + 1)
+            pmf = ((e** - lambtha) * (lambtha ** k))/ factorial
+            return pmf
+
+    def cdf(self, k):
+        '''
+        A method that calculates the value of the CDF for a given number of “successes”
+        '''
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+            cdf = 0
+            for i in range(k+1):
+                cdf += self.pmf(i)
+            return cdf
