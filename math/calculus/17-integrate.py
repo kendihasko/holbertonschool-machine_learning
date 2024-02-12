bec2fe1293ecfdaac679885b5728896a5dfad6dc
kendihasko <khasko16@epoka.edu.al>
2024-02-12 23:20:19 +0000

    Task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -9,8 +9,8 @@ def poly_integral(poly, C=0):
     A function that calculates the integral of a polynomial
     '''
     if not isinstance(poly, list) or \
-    not all(isinstance(coeff, (int, float)) for coeff in poly) or \
-    not isinstance(C, int):
+        not all(isinstance(coeff, (int, float)) for coeff in poly) or \
+        not isinstance(C, int):
             return None
 
     while poly and poly[-1] == 0:
