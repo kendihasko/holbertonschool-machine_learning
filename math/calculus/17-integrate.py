a7b51828b94bd95a4bfebc40f7ec61a6c96d4226
kendihasko <khasko16@epoka.edu.al>
2024-02-12 22:46:03 +0000

    Task 17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+A script that calculates the integral of a polynomial
+"""
+
+
+def poly_integral(poly, C=0):
+    '''
+    A function that calculates the integral of a polynomial
+    '''
+    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly) or not isinstance(C, int):
+            return None
+
+        while poly and poly[-1] == 0:
+            poly.pop()
+
+        if not poly:
+            return [C]
+
+        integral_coeffs = [poly[i] / (i + 1) for i in range(len(poly))]
+
+        integral_coeffs.insert(0, C)
+
+        return integral_coeffs
