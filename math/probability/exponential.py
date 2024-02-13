a9ffc81e60cc177bb219dd3ba19cff8ce8c1ac35
kendihasko <khasko16@epoka.edu.al>
2024-02-13 02:16:05 +0000

    Task 3

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -11,15 +11,13 @@ A class that represents an exponential distribution
     def __init__(self, data=None, lambtha=1.):
         self.lambtha = float(lambtha)
 
-        if self.lambtha < 0:
+        if self.lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
         if data is None:
             data = self.lambtha
         else:
-            
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = 1 / (float(sum(data)) / len(data))
-
