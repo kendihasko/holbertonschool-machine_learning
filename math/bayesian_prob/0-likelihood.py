28eaba17a6140c2b8ce56cea97b41ec12778493d
kendihasko <khasko16@epoka.edu.al>
2024-02-20 15:23:57 +0000

    Task 0

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''
+A script that calculates the likelihood of obtaining this data given 
+various hypothetical probabilities of developing severe side effects
+'''
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    '''
+    A function that calculates the likelihood of obtaining this data given 
+    various hypothetical probabilities of developing severe side effects
+    '''
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(p) is not into or x < 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray or len(p.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    for value in P:
+        if value > 1 or value < 0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+
+    factorial = np.math.factorial
+    fact_coefficient = factorial(n) / (factorial(x) * factorial(n - x))
+    likelihood = fact_coefficient * (P ** x) * (1 - P) ** (n - x)
+
+    return likelihood
