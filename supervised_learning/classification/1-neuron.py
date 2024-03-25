cfc11d786b0e61e65dca9def89c896757dc05231
kendihasko <khasko16@epoka.edu.al>
2024-03-25 14:58:10 +0000

    Task 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+'''
+Neuron class that defines a single neuron performing binary classification
+'''
+
+ 
+import numpy as np 
+
+
+class Neuron:
+    '''
+    A single neuron performing binary classification
+    '''
+
+
+    def __init__(self, nx):
+            '''
+            Class constructor
+            '''
+            if type(nx) is not int:
+                raise TypeError("nx must be an integer")
+            if nx < 1:
+                raise ValueError("nx must be a positive integer")
+            self.__W = np.random.randn(1, nx)
+            self.__b = 0
+            self.__A = 0
+            
+    @property
+    def W(self):
+        '''
+        Get method for property Weights
+        '''
+        return self.__W
+
+    @property
+    def b(self):
+        '''
+        Get method for property bias
+        '''
+        return self.__b
+
+    @property
+    def A(self):
+        '''
+        Get method for property prediction/output
+        '''
+        return self.__A
+
+
