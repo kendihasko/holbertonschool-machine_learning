e3128a829dbf73fa4dcaca82c726d9a548161a07
kendihasko <khasko16@epoka.edu.al>
2024-03-28 16:00:52 +0000

    Fixes

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -86,7 +86,7 @@ class NeuralNetwork:
         z1 = np.matmul(self.W1, X) + self.b1
         self.__A1 = 1 / (1 + np.exp(-z1))
 
-        z2 = np.matmul(self.W2, A1) + self.b2
+        z2 = np.matmul(self.W2, self.A1) + self.b2
         self.__A2 = 1 / (1 + np.exp(-z2))
 
         return (self.__A1, self.__A2)
