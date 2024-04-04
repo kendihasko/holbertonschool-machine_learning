6d46c66d101fcd34e5d7cf25a4f6e3c1c920b6dd
kendihasko <khasko16@epoka.edu.al>
2024-04-04 13:57:30 +0000

    Task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''
-Neuron class that defines a neural network with one hidden layer performing binary classification
+Neuron class that defines a neural network
+with one hidden layer performing binary classification
 '''
 
 
@@ -33,7 +34,7 @@ class DeepNeuralNetwork:
 
             weights["b{}".format(index)] = np.zeros((layer, 1))
             weights["W{}".format(index)] = (
-                np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            np.random.randn(layer, previous) * np.sqrt(2 / previous))
             previous = layer 
 
         self.L = len(layers)
