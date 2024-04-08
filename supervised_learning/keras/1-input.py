aa4e4631a62d29ff8277be627104dbef16919a7e
kendihasko <khasko16@epoka.edu.al>
2024-04-08 14:16:36 +0000

    Task 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -15,11 +15,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     x = inputs
     for i in range(len(layers)):
         x = K.layers.Dense(layers[i],
-                        activation=activations[i],
-                        kernel_regularizer=K.regularizers.L2(lambtha)(x)
+                           activation=activations[i],
+                           kernel_regularizer=K.regularizers.L2(lambtha))(x)
 
         if i != len(layers) - 1 and keep_prob is not None:
-            x = K.layers.Dropout(1-keep_prob)(x)
+            x = K.layers.Dropout(1 - keep_prob)(x)
 
-        model = K.Model(inputs, x)
-        return model
+    model = K.Model(inputs, x)
+
+    return model
