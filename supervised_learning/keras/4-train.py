37b9ad2fc45718393eb6b0225797c053efa7fcaa
kendihasko <khasko16@epoka.edu.al>
2024-04-08 14:45:15 +0000

    Task 4

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -11,3 +11,11 @@ def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle
     '''
     A function that trains a model using mini-batch gradient descent
     '''
+    history = model.fit(x=data,
+                        y=labels,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=verbose,
+                        shuffle=shuffle)
+
+    return history
