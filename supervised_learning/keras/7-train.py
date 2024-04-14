6782762c9ec5dab018e6f4dc7b705f6055cca884
kendihasko <khasko16@epoka.edu.al>
2024-04-14 09:47:31 +0000

    Updating file

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -16,29 +16,29 @@ def train_model(network, data, labels, batch_size,
     callback = []
     if early_stopping is True and validation_data is not None:
         early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
-                                            patience=patience)
+            patience=patience)
 
 
-        callback.append(early_stop)
+    callback.append(early_stop)
 
-        if learning_rate_decay and validation_data:
-            def scheduler(epochs):
-                lr = alpha / (1 + decay_rate * epochs)
-                return lr
+    if learning_rate_decay and validation_data:
+        def scheduler(epochs):
+            lr = alpha / (1 + decay_rate * epochs)
+            return lr
 
-            inv_time_decay = K.callbacks.LearningRateScheduler(
-                scheduler,
-                verbose=1)
+    inv_time_decay = K.callbacks.LearningRateScheduler(
+        scheduler,
+        verbose=1)
 
-            callback.append(inv_time_decay)
+    callback.append(inv_time_decay)
 
-        history = network.fit(x=data,
-            y=labels,
-            epochs=epochs,
-            batch_size=batch_size,
-            validation_data=validation_data,
+    history = network.fit(x=data,
+        y=labels,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=validation_data,
             callbacks=[callback],
             verbose=verbose,
             shuffle=shuffle)
 
-        return history
+    return history
