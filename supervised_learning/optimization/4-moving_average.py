6a1327697fca429e80e434f0d3a61c09824ab62b
kendihasko <khasko16@epoka.edu.al>
2024-04-22 22:05:22 +0000

    Update

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -6,17 +6,18 @@ Calculates the weighted moving average of a data set
 import numpy as np
 
 
-def moving_average(data, beta): 
+def moving_average(data, beta):
     '''
     A function that calculates the weighted moving average of a data set
     '''
-    m_av=[]
+    m_av = []
 
     w = 0
 
     for i, d in enumerate(data):
-        w = beta * w + (1+beta) * d
-        w_new = w/(i-beta**(i+1))
+        w = beta * w + (1 - beta) * d
+
+        w_new = w / (1 - beta ** (i + 1))
         m_av.append(w_new)
 
     return m_av
