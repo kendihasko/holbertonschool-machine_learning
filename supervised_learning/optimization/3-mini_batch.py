9cf4fb76fbb0f9eff4ace12b257e8736de72b780
kendihasko <khasko16@epoka.edu.al>
2024-04-22 22:00:21 +0000

    Update

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''
-Mini-batch
-'''
+"""
+   Mini-batch
+"""
 
 import tensorflow.compat.v1 as tf
 
@@ -9,27 +9,32 @@ shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
-                    epochs=5, load_path="/tmp/model.ckpt",
-                    save_path="/tmp/model.ckpt"):
-    '''
-    Function trains a loaded neural network model using
-    mini-batch gradient descent
-    '''
+                     epochs=5, load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+    """
+        Function trains a loaded neural network model using
+        mini-batch gradient descent
+    """
 
+    # metagraph and restore session
     with tf.Session() as sess:
         new_saver = tf.train.import_meta_graph(load_path + ".meta")
         new_saver.restore(sess, load_path)
 
+        # get following tensors and op from collection restored
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
         train_op = tf.get_collection('train_op')[0]
 
+        # store m
         m = X_train.shape[0]
 
+        # loop over epochs
         for epoch in range(epochs + 1):
 
+            # Print training and validation metrics after each epoch
             train_cost, train_accuracy = sess.run(
                 [loss, accuracy], feed_dict={x: X_train, y: Y_train})
 
@@ -46,6 +51,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
                 X_train_shuffled, Y_train_shuffled = \
                     shuffle_data(X_train, Y_train)
 
+                # calculate number of batches
                 nbr_batch = m // batch_size + (m % batch_size != 0)
 
                 for step_number in range(nbr_batch):
@@ -55,8 +61,10 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
                     x_batch = X_train_shuffled[first_index: last_index]
                     y_batch = Y_train_shuffled[first_index: last_index]
 
+                    # run training step with mini-batch
                     sess.run(train_op, feed_dict={x: x_batch, y: y_batch})
 
+                    # print mini-batch result every 100 batches
                     if step_number > 0 and (step_number + 1) % 100 == 0:
                         step_cost, step_accuracy = sess.run(
                             [loss, accuracy],
@@ -66,6 +74,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
                         print("\t\tCost: {}".format(step_cost))
                         print("\t\tAccuracy: {}".format(step_accuracy))
 
+        # save trained model
         saved_model = new_saver.save(sess, save_path)
 
         return saved_model
