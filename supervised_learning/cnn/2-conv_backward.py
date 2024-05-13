fae8441bacc113abe4a4d70f42b891086c74c950
kendihasko <khasko16@epoka.edu.al>
2024-05-13 13:53:08 +0000

    Updating file

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -8,21 +8,8 @@ import numpy as np
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """
-        function that performs back propagation over a convolutional
+        Avfunction that performs back propagation over a convolutional
         layer of NN
-
-        :param dZ: ndarray, shape(m,h_new,w_new_c_new) partial derivatives
-        with respect
-         to the unactivated output of the convolutional layer
-        :param A_prev: ndarray, shape(m,h_prev,w_prev,c_prev) output of
-        prev layer
-        :param W: ndarray, shape(kh,kw,c_prev,c_new) kernel convolution
-        :param b: ndarray, shape(1,1,1,c_new) biases
-        :param padding: string 'same' or 'valid' type of padding
-        :param stride: tuple (sh,sw) stride of convolution
-
-        :return: the partial derivatives with respect to the previous
-    layer (dA_prev),the kernels (dW), and the biases (db), respectively
     """
 
     # extract variable
