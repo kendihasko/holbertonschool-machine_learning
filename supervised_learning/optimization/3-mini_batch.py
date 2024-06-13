4c21bba815eb4a96d06fcf154958fec60a11272b
kendihasko <khasko16@epoka.edu.al>
2024-06-13 13:52:31 +0000

    Updating Task 3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -7,6 +7,7 @@ using mini-batch gradient descent
 import numpy as np
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
+
 def create_mini_batches(X, Y, batch_size):
     '''
     Creates mini-batches to be used for training
