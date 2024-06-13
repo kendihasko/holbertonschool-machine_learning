e64d672cb34a695ecf2a157f53e0b292082a9fda
kendihasko <khasko16@epoka.edu.al>
2024-06-13 13:49:52 +0000

    Updating Task 3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
-"""
+'''
 Defines function that trains a loaded neural network model
 using mini-batch gradient descent
-"""
+'''
 
-
-import tensorflow as tf
+import numpy as np
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
-
 def create_mini_batches(X, Y, batch_size):
-    """
+    '''
     Creates mini-batches to be used for training
     a neural network using mini-batch gradient descent.
-
-    Args:
-        X (numpy.ndarray): Input data of shape (m, nx).
-        Y (numpy.ndarray): Labels of shape (m, ny).
-        batch_size (int): Number of data points in a batch.
-
-    Returns:
-        list: List of mini-batches containing tuples (X_batch, Y_batch).
-    """
+    '''
     m = X.shape[0]
     mini_batches = []
 
