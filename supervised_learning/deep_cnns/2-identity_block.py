168de8868e31321d1c0d67ba89be8f0bfbdbceee
kendihasko <khasko16@epoka.edu.al>
2024-06-16 14:12:42 +0000

    Updating files

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -14,7 +14,6 @@ def identity_block(A_prev, filters):
     init = K.initializers.he_normal()
     activation = K.activations.relu
 
-    # First component of the main path
     C11 = K.layers.Conv2D(filters=F11,
                           kernel_size=(1, 1),
                           padding='same',
@@ -22,7 +21,6 @@ def identity_block(A_prev, filters):
     Batch_Norm11 = K.layers.BatchNormalization(axis=3)(C11)
     ReLU11 = K.layers.Activation(activation)(Batch_Norm11)
 
-    # Second component of the main path
     C3 = K.layers.Conv2D(filters=F3,
                          kernel_size=(3, 3),
                          padding='same',
@@ -30,14 +28,12 @@ def identity_block(A_prev, filters):
     Batch_Norm3 = K.layers.BatchNormalization(axis=3)(C3)
     ReLU3 = K.layers.Activation(activation)(Batch_Norm3)
 
-    # Third component of the main path
     C12 = K.layers.Conv2D(filters=F12,
                           kernel_size=(1, 1),
                           padding='same',
                           kernel_initializer=init)(ReLU3)
     Batch_Norm12 = K.layers.BatchNormalization(axis=3)(C12)
 
-    # Add shortcut value to the main path, and pass it through a RELU activation
     Addition = K.layers.Add()([Batch_Norm12, A_prev])
     output = K.layers.Activation(activation)(Addition)
 
