c85ae64e6cf3709bc295aaefd58c8ca5e2f7e9d8
kendihasko <khasko16@epoka.edu.al>
2024-06-16 14:04:57 +0000

    Updating files

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -6,7 +6,6 @@ Defines a function that builds an identity block using Keras
 
 from tensorflow import keras as K
 
-
 def identity_block(A_prev, filters):
     """
     Builds an identity block using Keras
