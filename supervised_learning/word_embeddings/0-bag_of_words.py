6810fd1a73b061845e39337382456cffb4068869
kendihasko <khasko16@epoka.edu.al>
2024-09-18 17:23:04 +0000

    Updating Task 0

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -8,6 +8,7 @@ import re
 import string
 from sklearn.feature_extraction.text import CountVectorizer
 
+
 def preprocess_text(text):
     '''
     Convert text to lowercase and replace punctuation with spaces.
@@ -16,21 +17,24 @@ def preprocess_text(text):
     text = re.sub(r'[{}]+'.format(re.escape(string.punctuation)), ' ', text)
     return text
 
+
 def bag_of_words(sentences, vocab=None):
     '''
     Creates a Bag of Words embedding matrix
     '''
     # Create a CountVectorizer instance with custom preprocessing
-    vectorizer = CountVectorizer(preprocessor=preprocess_text, vocabulary=vocab)
-    
+    vectorizer = CountVectorizer(
+        preprocessor=preprocess_text, vocabulary=vocab
+        )
+
     # Fit and transform the sentences
     embeddings = vectorizer.fit_transform(sentences).toarray()
-    
+
     # Get the feature names (vocabulary)
     features = vectorizer.get_feature_names_out()
-    
+
     # Format the features list
     formatted_features = ' '.join(f"'{item}'" for item in features)
     formatted_features = f"[{formatted_features}]"
-    
+
     return embeddings, formatted_features
