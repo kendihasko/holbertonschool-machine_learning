8a0a787b4662e071203e0254e54e10b3d36959b3
kendihasko <khasko16@epoka.edu.al>
2024-09-18 17:28:17 +0000

    Updating Task 1

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -15,7 +15,7 @@ def tf_idf(sentences, vocab=None):
     # Initialize the TfidfVectorizer with the given vocabulary if provided
     vectorizer = TfidfVectorizer(vocabulary=vocab)
 
-    # Fit the vectorizer on the sentences and transform the sentences to a TF-IDF matrix
+    # Fit the vectorizer on the sentences, transform sentences to a TF-IDF mtx
     tfidf_matrix = vectorizer.fit_transform(sentences)
 
     # Convert the sparse matrix to a dense numpy array
