e625c66a832b345ebe430bca16c4a490c2c69056
kendihasko <khasko16@epoka.edu.al>
2024-09-25 20:35:30 +0000

    Adding Files

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-"""Bayesian Information Criterion Module"""
+"""
+finds the best number of clusters for GMM using the Bayesian information
+Criterion
+"""
+
 import numpy as np
+
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """ Finds the best number of clusters for a GMM using the Bayesian
-    Information Criterion """
-
+    """
+    finds the best number of clusters for GMM using the Bayesian information
+    Criterion
+    """
     try:
         if kmax == 1:
             return None, None, None, None
@@ -17,12 +23,12 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
             if kmax >= kmin:
                 return None, None, None, None
 
-        k_history = list(range(kmin, kmax+1))
-        results_history = []
-        lh_history = []
-        bic_history = []
+        k_hist = list(range(kmin, kmax + 1))
+        results_hist = []
+        lh_hist = []
+        bic_hist = []
 
-        for k in range(kmin, kmax+1):
+        for k in range(kmin, kmax + 1):
             pi, m, S, g, lh = expectation_maximization(X, k, iterations, tol,
                                                        verbose)
 
@@ -32,14 +38,15 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
             num_parameters = k + k * d + k * d * (d + 1) // 2 - 1
             bic = num_parameters * np.log(n) - 2 * lh
 
-            lh_history.append(lh)
-            results_history.append((pi, m, S))
-            bic_history.append(bic)
+            lh_hist.append(lh)
+            results_hist.append((pi, m, S))
+            bic_hist.append(bic)
 
-        min_bic_index = np.argmin(bic_history)
-        best_k = k_history[min_bic_index]
-        best_result = results_history[min_bic_index]
+            min_bic_index = np.argmin(bic_hist)
+            best_k = k_hist[min_bic_index]
+            best_result = results_hist[min_bic_index]
 
-        return best_k, best_result, np.array(lh_history), np.array(bic_history)
+            return best_k, best_result, np.array(lh_hist), np.array(bic_hist)
+
     except Exception:
         return None, None, None, None
