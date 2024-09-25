44d1c5f91746dee3f81123c898d7a38524891935
kendihasko <khasko16@epoka.edu.al>
2024-09-25 20:30:47 +0000

    Adding Files

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -4,9 +4,10 @@
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
+
 def initialize(X, k):
     """
-    Initializes variables for a Gaussian Mixture Model
+    initializes variables for a Gaussian Mixture Model
     """
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
@@ -16,19 +17,13 @@ def initialize(X, k):
 
     n, d = X.shape
 
-    # Priors for each cluster, initialized evenly
+    # priors for each cluster, initialized evenly
     phi = np.ones(k) / k
 
-    # Centroid means for each cluster, initialized with K-means
+    # centroid means for each cluster, initialized with K-means
     m, _ = kmeans(X, k)
 
-    # Covariance matrices for each cluster, initialized as identity matrices
+    # covariance matrices for each cluster, initialized as identity matrices
     S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
-
-    # While loop for demonstration; it just runs once here
-    count = 0
-    while count < 1:
-        # This loop doesn't serve a purpose but demonstrates the usage of while
-        count += 1
 
     return phi, m, S
